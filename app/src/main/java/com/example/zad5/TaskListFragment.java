f179e60e6edd8750abb71a94d9b5a0ad0d085cc2
pawelsiemieniuk <pawel.siemieniuk11@gmail.com>
2025-02-07 15:58:24 +0100

    DONE

--- app/src/main/java/com/example/zad5/TaskListFragment.java
+++ app/src/main/java/com/example/zad5/TaskListFragment.java
@@ -18,7 +18,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
